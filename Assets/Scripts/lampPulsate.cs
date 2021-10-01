679f2312cd5aa3412a32c5dcdffef661819a73d1
Grigory Latyshev <thegriglat@gmail.com>
2021-10-02 00:19:40 +0300

    village

--- Assets/Scripts/lampPulsate.cs
+++ Assets/Scripts/lampPulsate.cs
@@ -12,18 +12,20 @@ public class lampPulsate : MonoBehaviour
     public float speed = 1.0f;
 
     private Light2D l;
+    private int sign = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         l = GetComponent<Light2D>();
+        if (maxIntensity < minIntensity) sign = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
         l.intensity =
-            minIntensity +
-            Mathf.PingPong(Time.time * speed, maxIntensity - minIntensity);
+            minIntensity + sign * 
+            Mathf.PingPong(Time.time * speed, Mathf.Abs(maxIntensity - minIntensity));
     }
 }
